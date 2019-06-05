917bfd062bbfa48184a0ea84e041eec387c05820
deivijt <davidjaimest@gmail.com>
2019-06-05 14:50:13 -0500

    feat: add reverse string exercise

---
+++ exercises/reverse_string/index.js
@@ -0,0 +1,36 @@
+/**
+ * --- Directions:
+ * Given a string, return a new string with the reversed order of characters
+ * --- Examples:
+ * reverse('apple') === 'elppa'
+ * reverse('hello') === 'olleh'
+ * reverse('Greetings!') === '!sgniteerG'
+ */
+
+// Solution #1
+function reverse(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+// Solution #2
+function reverse2(str) {
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// Solution #3
+function reverse3(str) {
+  return str.split("").reduce((accumulator, character) => {
+    return character + accumulator;
+  }, "");
+}
+
+module.exports = reverse3;
